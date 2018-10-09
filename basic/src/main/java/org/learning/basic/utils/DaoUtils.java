302f774038b4294a9b4d563b1b4c9219286a6f44
dean <278245725@qq.com>
2018-10-09 18:20:34 +0800

    DISTINCT count

--- basic/src/main/java/org/learning/basic/utils/DaoUtils.java
+++ basic/src/main/java/org/learning/basic/utils/DaoUtils.java
@@ -1,26 +1,27 @@
 package org.learning.basic.utils;
 
-import org.springframework.util.Assert;
-
 public abstract class DaoUtils {
 
-	private static final String FROM = "FROM ";
-	private static final String ORDER = "ORDER ";
+    private static final String SELECT = "SELECT ";
+    private static final String FROM = "FROM ";
+    private static final String DISTINCT = "DISTINCT ";
+    private static final String ORDER = "ORDER ";
 
-	public static String count(String sql) {
-		String uql = StringUtils.upperCase(sql);
-		int index = uql.indexOf(FROM);
-		String cql = count(sql, index);
-		Assert.isTrue(uql.substring(0, index).indexOf(" distinct ") == -1, "The sql select field contains distinct, count all will be wrong: " + sql + " -> " + cql);
-		return cql;
-	}
-
-	private static String count(String sql, int index) {
-	    sql = sql.substring(index + FROM.length());
-	    String uql = StringUtils.upperCase(sql);
-		if ((index = uql.indexOf(ORDER)) != -1) {
-		    sql = sql.substring(0, index);
-		}
-		return "select count(1) from " + sql;
-	}
+    public static String count(String sql) {
+        String uql = StringUtils.upperCase(sql);
+        int index = uql.indexOf(FROM);
+        if (uql.indexOf(DISTINCT) > 0) {
+            String cql = "select count(" + sql.substring(SELECT.length(), index) + ") " + sql.substring(index);
+            if ((index = uql.indexOf(ORDER)) != -1) {
+                cql = cql.substring(0, index);
+            }
+            return cql;
+        } else {
+            sql = sql.substring(index + FROM.length());
+            if ((index = uql.indexOf(ORDER)) != -1) {
+                sql = sql.substring(0, index);
+            }
+            return "select count(1) from " + sql;
+        }
+    }
 }
